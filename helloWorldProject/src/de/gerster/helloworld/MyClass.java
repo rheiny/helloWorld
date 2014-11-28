833a378e89521bb5b827464e84183af76d63801d
Reinhard Gerster <Reinhard.Gerster@bosch-si.com>
2014-11-28 10:11:28 +0100

    new class added

---
+++ helloWorldProject/src/de/gerster/helloworld/MyClass.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2014 Bosch Software Innovations GmbH. All rights reserved.
+ */
+
+package de.gerster.helloworld;
+
+public class MyClass
+{
+   private String name;
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+}
