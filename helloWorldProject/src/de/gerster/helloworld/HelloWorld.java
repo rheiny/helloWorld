241787fa4f2134cd0a80f791b1a7241eef647d9e
Reinhard Gerster <Reinhard.Gerster@bosch-si.com>
2014-11-28 10:15:38 +0100

    other line deleted

--- helloWorldProject/src/de/gerster/helloworld/HelloWorld.java
+++ helloWorldProject/src/de/gerster/helloworld/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld
 
    public static void main(String[] args)
    {
-      System.out.println("hello world");
+
       System.out.println("i need some business logic");
    }
 }
