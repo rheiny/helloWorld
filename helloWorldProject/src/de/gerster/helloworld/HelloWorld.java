40f3de890d7000155a77204f0273ca0a046d4de4
Reinhard Gerster <Reinhard.Gerster@bosch-si.com>
2014-11-28 09:28:50 +0100

    init helloWorld

---
+++ helloWorldProject/src/de/gerster/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2014 Bosch Software Innovations GmbH. All rights reserved.
+ */
+
+package de.gerster.helloworld;
+
+public class HelloWorld
+{
+
+   public static void main(String[] args)
+   {
+      System.out.println("hello world");
+
+   }
+
+}
