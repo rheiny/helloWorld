e2c8c57a8856d991e74f543ec12a13fc5785491d
Reinhard Gerster <Reinhard.Gerster@bosch-si.com>
2014-11-28 09:41:38 +0100

    business logic added

--- helloWorldProject/src/de/gerster/helloworld/HelloWorld.java
+++ helloWorldProject/src/de/gerster/helloworld/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld
    public static void main(String[] args)
    {
       System.out.println("hello world");
-
+      System.out.println("i need some business logic");
    }
 
 }
