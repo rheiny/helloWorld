13baf58062dc95d5ffa92c28d21aa4dea1e993fa
r6 <rheiny@gmx.de>
2015-11-23 21:54:13 +0100

    hello reiny

--- helloWorldProject/src/de/gerster/helloworld/HelloWorld.java
+++ helloWorldProject/src/de/gerster/helloworld/HelloWorld.java
@@ -4,11 +4,9 @@
 
 package de.gerster.helloworld;
 
-public class HelloWorld
-{
+public class HelloWorld {
 
-   public static void main(String[] args)
-   {
-      System.out.println("hello world");
-   }
+	public static void main(String[] args) {
+		System.out.println("hello reiny");
+	}
 }
